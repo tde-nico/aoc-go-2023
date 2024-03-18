ea3bbb3ccfc95d06103f5e1d8484de23704fee27
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-03-18 23:37:42 +0100

    day04

---
+++ day04/part01/go.mod
@@ -0,0 +1,3 @@
+module aoc4
+
+go 1.21.0
---
+++ day04/part01/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Card struct {
+	winning map[int64]bool
+	chosen  map[int64]bool
+}
+
+func (c *Card) count() int64 {
+	var count int64 = 0
+	for win := range c.winning {
+		if c.chosen[win] {
+			count++
+		}
+	}
+	return count
+}
+
+func (c *Card) score() int64 {
+	cards := c.count()
+	if cards > 0 {
+		return 1 << (cards - 1)
+	}
+	return 0
+}
+
+type Runner struct {
+	cards []Card
+}
+
+func (r *Runner) parse() {
+	r.cards = make([]Card, 0)
+	reader := bufio.NewReader(os.Stdin)
+	for row := 0; ; row++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			fmt.Printf("Reader Error: %s\n", err)
+			return
+		}
+
+		data := strings.Split(line, ": ")
+		nums := data[1]
+		cards := strings.Split(nums, " | ")
+		win, chose := cards[0], cards[1]
+
+		winning := make(map[int64]bool)
+		for _, stringValue := range strings.Fields(win) {
+			value, err := strconv.Atoi(stringValue)
+			if err != nil {
+				fmt.Printf("Error converting string to int: %v\n", err)
+				return
+			}
+			winning[int64(value)] = true
+		}
+
+		chosen := make(map[int64]bool)
+		for _, stringValue := range strings.Fields(chose) {
+			value, err := strconv.Atoi(stringValue)
+			if err != nil {
+				fmt.Printf("Error converting string to int: %v\n", err)
+				return
+			}
+			chosen[int64(value)] = true
+		}
+
+		r.cards = append(r.cards, Card{winning, chosen})
+	}
+}
+
+func (r *Runner) part1() {
+	var total int64 = 0
+	for _, card := range r.cards {
+		total += card.score()
+	}
+	fmt.Printf("%v\n", total)
+}
+
+func main() {
+	r := Runner{}
+	r.parse()
+	r.part1()
+}
